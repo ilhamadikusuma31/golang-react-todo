6cb03acab3e68935b0928fdbb9fd60215cbf34f0
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-08-01 00:08:59 +0700

    modified:   server/main.go 	new file:   server/models/models.go

--- server/main.go
+++ server/main.go
@@ -1,1 +1,13 @@
-package main	
+package main
+
+import (
+	"fmt"
+	"golang-react-todo/router"
+	"net/http"
+)
+
+func main(){
+	r := router.Router()
+	fmt.Println("")
+	http.ListenAndServe(":9000",r)
+}
