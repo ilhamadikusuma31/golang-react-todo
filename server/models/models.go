a49f20bad3584d4d9fcac2da22a8ec4f1b7f5bd4
ilhamadikusuma31 <ilhamadikusuma31@gmail.com>
2022-08-01 00:58:38 +0700

    router

--- server/models/models.go
+++ server/models/models.go
@@ -5,7 +5,7 @@ import (
 )
 
 type ToDoList struct{
-	ID     		primitive.ObjectID `json:"_id",omitempty` `bson:"_id",omitempty`
+	ID     		primitive.ObjectID `json:"_id",omitempty``bson:"_id",omitempty`
 	Task    	string  `json:"task",omitempty`
 	Status   	bool    `json:"status",omitempty`
 }
--- server/router/routes.go
+++ server/router/routes.go
@@ -1,1 +1,17 @@
 package router
+
+import (
+	"golang-react-todo/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router{
+	router := mux.NewRouter()
+	router.HandleFunc("api/task", middleware.GetAllTasks).Methods("GET","OPTIONS")
+	router.HandleFunc("api/tasks",middleware.CreateTask).Methods("POST","OPTIONS")
+	router.HandleFunc("api/tasks/{id}",middleware.TaskComplete).Methods("PUT","OPTIONS")
+	router.HandleFunc("api/undoTasks/{id}",middleware.UndoTask).Methods("PUT","OPTIONS")
+	router.HandleFunc("api/deleteTask/{id}",middleware.DeleteTask).Methods("DELETE","OPTIONS")
+	router.HandleFunc("api/deleteAllTasks/{id}",middleware.DeleteTask).Methods("DELETE","OPTIONS")
+}
